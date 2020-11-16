fb3fbcb2c660ff493d1dcb36a42d3ec276cb302a
coord-geom <ohzhiyuan1010@gmail.com>
2020-11-16 21:57:19 +0800

    Made everything have some CSS

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("css", views.css, name="CSS"),
-    path("django", views.css, name="Django"),
-    path("git", views.css, name="Git"),
-    path("html", views.css, name="HTML"),
-    path("python", views.css, name="Python")
+    path("create", views.create, name="create"),
+    path("random", views.random, name="random"),
+    path("wiki/<str:title>", views.title, name="title")
+    #path("wiki/CSS", views.CSS, name="CSS"),
+    #path("wiki/Django", views.Django, name="Django"),
+    #path("wiki/Git", views.Git, name="Git"),
+    #path("wiki/HTML", views.HTML, name="HTML"),
+    #path("wiki/Python", views.Python, name="Python"),  
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,37 +1,46 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from markdown2 import markdown
+
+from random import shuffle
+
 from . import util
 
+renderHTML = 'encyclopedia/load.html'
+loadHTML = 'encyclopedia/templates/encyclopedia/load.html'
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def css(request):
-    return render(request, "encyclopedia/css.html", {
-        "entries": util.list_entries()
+def create(request):
+    return render(request, "encyclopedia/create.html", {
+        
     })
 
-def django(request):
-    return render(request, "encyclopedia/django.html", {
-        "entries": util.list_entries()
-    })
+def load_page(request, title):
+    string = '{% extends "encyclopedia/layout.html" %}{% block title %}' + \
+                title + '{% endblock %}{% block body %}' + markdown(util.get_entry(title)) + '{% endblock %}'
+    file = open("encyclopedia/templates/encyclopedia/load.html", "w")
+    file.write(string)
+    file.close()
+    return render(request, "encyclopedia/load.html")
 
-def git(request):
-    return render(request, "encyclopedia/git.html", {
-        "entries": util.list_entries()
-    })
+def random(request):
+    entries = util.list_entries()
+    shuffle(entries)
+    return load_page(request, entries[0])
 
-def html(request):
-    return render(request, "encyclopedia/html.html", {
-        "entries": util.list_entries()
-    })
+def title(request, title):
+    string = util.get_entry(title)
+    if string == None:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return load_page(request, title)
 
-def python(request):
-    return render(request, "encyclopedia/python.html", {
-        "entries": util.list_entries()
-    })
 
 
 
