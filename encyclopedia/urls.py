3d5eb7806235261009c15aab7551c9f71e781c3f
coord-geom <ohzhiyuan1010@gmail.com>
2020-12-02 00:50:02 +0800

    implemented create page and random page completely

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path("create", views.create, name="create"),
     path("random", views.random, name="random"),
     path("wiki/<str:title>", views.title, name="title")
-    #path("wiki/CSS", views.CSS, name="CSS"),
-    #path("wiki/Django", views.Django, name="Django"),
-    #path("wiki/Git", views.Git, name="Git"),
-    #path("wiki/HTML", views.HTML, name="HTML"),
-    #path("wiki/Python", views.Python, name="Python"),  
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
 from markdown2 import markdown
 
-from random import shuffle
+from random import shuffle, randrange
 
 from . import util
 
-renderHTML = 'encyclopedia/load.html'
-loadHTML = 'encyclopedia/templates/encyclopedia/load.html'
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Entry Name")
+    content = forms.CharField(widget=forms.Textarea, label="")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -17,29 +19,39 @@ def index(request):
     })
 
 def create(request):
-    return render(request, "encyclopedia/create.html", {
-        
-    })
+    if request.method == "GET":
+        return render(request, "encyclopedia/create.html", {
+            "form": NewEntryForm()
+        })
+    elif request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            if title in util.list_entries():
+                return render(request, "encyclopedia/error.html")
+            else:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse("title", kwargs={"title": title}))
 
-def load_page(request, title):
-    string = '{% extends "encyclopedia/layout.html" %}{% block title %}' + \
-                title + '{% endblock %}{% block body %}' + markdown(util.get_entry(title)) + '{% endblock %}'
-    file = open("encyclopedia/templates/encyclopedia/load.html", "w")
-    file.write(string)
-    file.close()
-    return render(request, "encyclopedia/load.html")
 
 def random(request):
     entries = util.list_entries()
-    shuffle(entries)
-    return load_page(request, entries[0])
+    x = randrange(0,len(entries))
+    # return page(request, entries[x])
+    return HttpResponseRedirect(reverse("title", kwargs={"title": entries[x]}))
 
 def title(request, title):
     string = util.get_entry(title)
     if string == None:
         return render(request, "encyclopedia/error.html")
     else:
-        return load_page(request, title)
+        string = '{% extends "encyclopedia/layout.html" %}{% block title %}' + \
+                title + '{% endblock %}{% block body %}' + markdown(util.get_entry(title)) + '{% endblock %}'
+        file = open("encyclopedia/templates/encyclopedia/load.html", "w")
+        file.write(string)
+        file.close()
+        return render(request, "encyclopedia/load.html")
 
 
 
